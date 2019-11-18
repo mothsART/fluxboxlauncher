f6dd4c11f4e5704462347305c4b1995c9e7d0c82
Ferry Jérémie <ferryjeremie@free.fr>
2019-11-18 23:28:31 +0100

    warning on duplicates

--- lib/i18n.py
+++ lib/i18n.py
@@ -11,7 +11,7 @@ _ = gettext.gettext
 
 _duplicate = _('Duplicate')
 _app_already_exists = _('This application already exists')
-                
+
 _confirmation = _('Confirmation of deletion')
 _confirm_question = _('Do you really want to delete this app from Fluxbox launch ?')
 
