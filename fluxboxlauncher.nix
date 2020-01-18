dbb7c7b3c97d55832091d3d76b797fb7566177b5
zimbatm <zimbatm@zimbatm.com>
2020-01-18 14:40:24 +0100

    fix the nix build
    * make default.nix the entry-point so you can just run `nix-build`
    * move the previous default.nix to fluxboxlauncher.nix, this could be
      copied into nixpkgs (with a different src attribute)
    * use lib.cleanSource to avoid rebuilds
    * add missing python dependency
    * format the code a bit to follow the nixpkgs style

---
+++ fluxboxlauncher.nix
@@ -0,0 +1,30 @@
+{ lib
+, python3
+, gtk3
+, gobject-introspection
+, buildPythonApplication ? python3.pkgs.buildPythonApplication
+}:
+
+buildPythonApplication rec {
+  pname = "fluxboxlauncher";
+  version = "0.1";
+
+  src = lib.cleanSource ./.;
+
+  buildInputs = [
+    gobject-introspection
+    gtk3
+  ];
+
+  propagatedBuildInputs = with python3.pkgs; [
+    pygobject3
+  ];
+
+  meta = with lib; {
+    description = "Fluxboxlauncher";
+    homepage = "https://github.com/mothsART/fluxboxlauncher";
+    maintainers = with maintainers; [ mothsart ];
+    license = licenses.bsdOriginal;
+    platforms = platforms.unix;
+  };
+}
