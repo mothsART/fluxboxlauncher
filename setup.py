e95afdc3e75c78f0c6ba78445a55a3d81db30957
Ferry Jérémie <ferryjeremie@free.fr>
2020-01-18 14:25:10 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='fluxboxlauncher',
+    version=0.1,
+    description=(
+        '...'
+    ),
+    author='mothsart',
+    author_email='ferryjeremie@free.fr',
+    url='https://github.com/mothsart/fluxboxlauncher'
+)
